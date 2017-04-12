159c5a0f47a0c93354293f4bae40e4e39ee10392
paulemboli <iampaulemboli@gmail.com>
2017-04-12 06:16:48 -0500

    Add files via upload

---
+++ ifElseStatement.java
@@ -0,0 +1,20 @@
+package Review.java;
+
+public class ifElseStatement {
+
+	public static void main(String[] args) {
+		
+		int userAge = 0;
+		
+		if (userAge <= 18){
+			System.out.println(" 18 or less ");
+			
+		} else{
+			System.out.println(" Over 18");
+			
+		}
+		return;
+		
+	}
+
+}
